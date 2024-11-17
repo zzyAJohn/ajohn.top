9830f3a40382713df4f4507195d68f58ff2dea10
zzyAJohn <1833302139@qq.com>
2024-11-17 11:40:38 +0800

    chore(config): update base path to '/ajohn.top/'

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from 'vuepress'
 import { plumeTheme } from 'vuepress-theme-plume'
 
 export default defineUserConfig({
-  base: '/',
+  base: '/ajohn.top/',
   lang: 'zh-CN',
   title: 'My Vuepress Site',
   description: '',
