b949ea63226f27b0bc3d61ff9967fedfecd8ea99
zzyAJohn <1833302139@qq.com>
2024-12-09 10:34:03 +0800

    refactor(notes): restructure leetcode notes

--- docs/.vuepress/notes/leetcode.ts
+++ docs/.vuepress/notes/leetcode.ts
@@ -6,12 +6,38 @@ export default defineNoteConfig({
   sidebar: [
     'README.md',
     {
-      text: '题单',
+      text: '一、滑动窗口与双指针',
+      prefix: '1.sliding-window', 
       items: [
-        '1.sliding-window.md',
-        '2.binary-algorithm.md',
-        '3.monotonic-stack.md',
-        '8.common-data-structures.md',
+        '1.fixed-length-sliding-window.md',
+        '2.variable-length-sliding-window.md',
+        '3.single-sequence-double-pointer.md',
+        '4.double-sequence-double-pointer.md',
+        '5.three-pointers.md',
+      ]
+    },
+    {
+      text: '二、二分算法',
+      prefix: '2.binary-algorithm', 
+      items: [
+        '1.binary-search.md',
+        '2.binary-answer.md',
+        '3.binary-indirect-value.md',
+        '4.minimize-the-maximum-value.md',
+      ]
+    },
+    {
+      text: '三、单调栈',
+      prefix: '3.monotonic-stack', 
+      items: [
+        'temp.md',
+      ]
+    },
+    {
+      text: '八、常用数据结构',
+      prefix: '8.common-data-structures', 
+      items: [
+        '0.common-enumeration-techniques.md',
       ]
     },
   ]
---
+++ docs/notes/leetcode/3.monotonic-stack/temp.md
@@ -0,0 +1,10 @@
+---
+title: 3.1 单调栈
+tags:
+    - Leetcode
+    - Python
+createTime: 2024/12/04 12:45:12
+permalink: /leetcode/z09vka85/
+---
+
+推荐先做做 [8.【题单】常用数据结构](../8.common-data-structures/0.common-enumeration-techniques.md) 中的「枚举右，维护左」再来刷本题单。
