8b9a2075fbb869ab5bb5bad28e4621cf1005aeb9
zzyAJohn <1833302139@qq.com>
2025-02-20 11:49:20 +0800

    docs: update reading notes, add literary and psychological classifications and related documents

--- docs/.vuepress/notes/read.ts
+++ docs/.vuepress/notes/read.ts
@@ -6,9 +6,16 @@ export default defineNoteConfig({
   sidebar: [
     'README.md',
     {
-      text: '在读',
+      text: '文学',
+      prefix: 'literature', 
       items: [
         'weicheng.md',
+      ]
+    },
+    {
+      text: '心理学',
+      prefix: 'psychology', 
+      items: [
         'How to Win Friends & Influence People.md',
       ]
     },
