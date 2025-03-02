834fc94b129f19c04badefe34858284be8955bda
zzyAJohn <1833302139@qq.com>
2025-03-02 22:08:18 +0800

    docs: update records, add the weekly journal for February 24, 2025 and adjust the date order

--- docs/.vuepress/notes/record.ts
+++ docs/.vuepress/notes/record.ts
@@ -9,9 +9,10 @@ export default defineNoteConfig({
       text: '2025',
       prefix: '2025', 
       items: [
-        '2025-02-03.md',
-        '2025-02-10.md',
+        '2025-02-24.md',
         '2025-02-17.md',
+        '2025-02-10.md',
+        '2025-02-03.md',
       ]
     },
   ]
