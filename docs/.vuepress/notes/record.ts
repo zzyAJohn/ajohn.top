dd2f213b85fe1c63cca1bbbe1c7cccc821fcdd38
zzyAJohn <1833302139@qq.com>
2025-05-06 20:09:16 +0800

    docs: update records for April and May 2025, add new weekly records and sidebar items

--- docs/.vuepress/notes/record.ts
+++ docs/.vuepress/notes/record.ts
@@ -6,9 +6,17 @@ export default defineNoteConfig({
   sidebar: [
     'README.md',
     {
+      text: '2025-05',
+      prefix: '2025-05', 
+      items: [
+        '2025-05-05.md',
+      ]
+    },
+    {
       text: '2025-04',
       prefix: '2025-04', 
       items: [
+        '2025-04-28.md',
         '2025-04-21.md',
         '2025-04-14.md',
         '2025-04-07.md',
