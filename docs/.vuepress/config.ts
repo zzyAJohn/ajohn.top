c3da8ea0776b5ce25e04c2f4e4c30f977d0cd1ce
zzyAJohn <1833302139@qq.com>
2025-06-24 12:44:12 +0800

    docs: add encryption rules for specific files and directories in VuePress configuration

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -15,6 +15,20 @@ export default defineUserConfig({
   bundler: viteBundler(),
 
   theme: plumeTheme({
+    encrypt: {
+      rules: {
+        // // 可以是 md 文件的相对路径，对该文件加密
+        // '前端/基础.md': '123456',
+        // // 可以是 文件夹的路径，对该目录下所有文章加密
+        '/cs61b/': '密码',
+        // // 可以是 访问地址的请求路径，对该访问路径下所有文章加密
+        // '/vuepress-theme-plume/': '123456',
+        // // 可以是 具体的某个页面的请求路径，对该页面加密
+        // '/article/f8dnci3/': '123456',
+        // // 如果是 `^` 开头，则匹配该正则表达式的页面也会加密
+        // '^/(a|b)/': '123456',
+      }
+    },
     markdown: {
       annotation: true, 
       bilibili: true, 
