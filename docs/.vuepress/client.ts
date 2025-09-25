b55d222e4c54b17f0fee6d815fef05726c213879
zzyAJohn <1833302139@qq.com>
2025-09-25 21:52:36 +0800

    feat: add custom components and update README configuration

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -1,14 +1,21 @@
 import { defineClientConfig } from 'vuepress/client'
 import RepoCard from 'vuepress-theme-plume/features/RepoCard.vue'
 import Swiper from 'vuepress-theme-plume/features/Swiper.vue'
-// import CustomComponent from './theme/components/Custom.vue'
+import './theme/styles/custom.css'
+import Custom from './theme/components/Custom.vue'
+import AllFriendContent from './theme/components/AllFriendContent.vue'  // 友情链接页面所需组件
+import ViewedCount from './theme/components/ViewedCount.vue'  
 
-// import './theme/styles/custom.css'
+import './theme/styles/custom.css'
 
 export default defineClientConfig({
   enhance({ app }) {
     app.component('RepoCard', RepoCard)
     app.component('Swiper', Swiper)
+    app.component('Custom', Custom)
+    app.component('AllFriendContent', AllFriendContent)  
+    app.component('ViewedCount', ViewedCount)  
+    // app.component('MyComponent', MyComponent)
     // app.component('CustomComponent', CustomComponent)
   },
 })
--- docs/README.md
+++ docs/README.md
@@ -2,21 +2,5 @@
 pageLayout: home
 externalLinkIcon: false
 config:
-  -
-    type: hero
-    full: true
-    background: tint-plate
-    hero:
-      name: AJohn Blog
-      tagline: Nothing for nothing
-      text: 「我希望正在读这句话的人永远开心」
-      actions:
-        -
-          theme: brand
-          text: 博客
-          link: /blog/
-        -
-          theme: alt
-          text: Github →
-          link: https://github.com/zzyAJohn
+  - type: Custom
 ---
