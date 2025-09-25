528c8704c5a819b58e147b51e04658af6da8900d
zzyAJohn <1833302139@qq.com>
2025-09-25 16:15:14 +0800

    docs: add icons to navbar items for improved navigation

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -1,14 +1,14 @@
 import { defineNavbarConfig } from 'vuepress-theme-plume'
 
 export const navbar = defineNavbarConfig([
-  { text: '首页', link: '/' },
-  { text: '博客', link: '/blog/' },
-  { text: '标签', link: '/blog/tags/' },
-  { text: '归档', link: '/blog/archives/' },
-  { text: '友链', link: '/friends/' },
-  { text: '工具', link: '/tools/' },
-  { text: '作品', link: '/notes/works/README.md' },
-  { text: '关于', link: '/about/' },
+  { text: '首页', link: '/', icon: 'material-symbols:home-rounded' },
+  { text: '博客', link: '/blog/', icon: 'material-symbols:article-rounded' },
+  { text: '标签', link: '/blog/tags/', icon: 'material-symbols:label-rounded' },
+  { text: '归档', link: '/blog/archives/', icon: 'material-symbols:folder-rounded' },
+  { text: '友链', link: '/friends/', icon: 'material-symbols:linked-services' },
+  { text: '工具', link: '/tools/', icon: 'material-symbols:build-rounded' },
+  { text: '作品', link: '/notes/works/README.md', icon: 'material-symbols:photo-camera-rounded' },
+  { text: '关于', link: '/about/', icon: 'material-symbols:emoji-people-rounded' },
   {
     text: '笔记',
     items: [
@@ -24,10 +24,10 @@ export const navbar = defineNavbarConfig([
     text: '日常',
     items: [
             // { text: '示例', link: '/notes/demo/README.md' },
-            { text: 'Read', link: '/notes/read/README.md' },
-            { text: 'Record', link: '/notes/record/README.md' },
-            { text: 'Music', link: '/notes/music/README.md' },
-            { text: 'TV', link: '/notes/tv/README.md' },
+            { text: 'Read', link: '/notes/read/README.md', icon: 'material-symbols:menu-book-rounded' },
+            { text: 'Record', link: '/notes/record/README.md', icon: 'material-symbols:ink-pen-rounded' },
+            { text: 'Music', link: '/notes/music/README.md', icon: 'material-symbols:music-note-rounded' },
+            { text: 'TV', link: '/notes/tv/README.md', icon: 'material-symbols:live-tv-rounded' },
             // { text: 'Plant', link: '/notes/plant/README.md' },
     ]
   },
