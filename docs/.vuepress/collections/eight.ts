f5ef80de0251ee5cf5e1f0ab9d0728a62dd94996
zzyAJohn <1833302139@qq.com>
2026-03-31 21:10:28 +0800

    docs: update the notes of the Eight legged Essay, remove the operating system section, and add computer network introduction and basic content

--- docs/.vuepress/collections/eight.ts
+++ docs/.vuepress/collections/eight.ts
@@ -13,21 +13,26 @@ export default defineCollection({
         'cpp.md',
       ]
     },
-    {
-      text: '操作系统',
-      prefix: 'os',
-      items: [
-        '1.preface.md',
-        '2.hardware.md',
-        '3.os-structure.md',
-        '4.memory.md',
-      ]
-    },
+    // {
+    //   text: '操作系统',
+    //   prefix: 'os',
+    //   items: [
+    //     '1.preface.md',
+    //     '2.hardware.md',
+    //     '3.os-structure.md',
+    //     '4.memory.md',
+    //   ]
+    // },
     {
       text: '计算机网络',
       prefix: 'network',
       items: [
-        'network.md',
+        '1.preface.md',
+        '2.base.md',
+        // '3.http.md',
+        // '4.tcp.md',
+        // '5.ip.md',
+        // '6.all.md',
       ]
     },
   ]
--- docs/eight/README.md
+++ docs/eight/README.md
@@ -4,4 +4,5 @@ permalink: /eight/
 createTime: 2025/10/28 17:55:07
 ---
 
-TODO：八股介绍
+
+本系列笔记主要是对八股内容的学习整理。
